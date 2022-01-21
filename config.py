176284eafa6793bbccbc65d07ecb744ea51a31f2
Mark20067 <98180771+Mark20067@users.noreply.github.com>
2022-01-21 22:55:05 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 SIGN_KEY = 'oc3q7ingf978mx457fgk4587fg847'
 RUCAPTCHA_KEY = ''
-NAME = ''
-USER_ID = 0
-TOKEN = ''
+NAME = Volkims54
+USER_ID = 9vTQfWjW
+TOKEN = 
 HUMAN_CAPTCHA_SOLVE = True
 level = 'INFO'
